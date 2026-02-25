ea70ad0012b8dff5b5ca67d6977b670ebd8e8089
Amithesh K <29amithesh@gmail.com>
2026-02-25 18:48:59 +0530

    initial test push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="p-10 max-w-2xl mx-auto prose prose-blue">
+      <h1>Ghost Editor Clone</h1>
+      <p>If this text looks nicely formatted, Tailwind v4 is working perfectly!</p>
+    </div>
+  )
+}
